4ae10c33ea14f7980e29059be52ff5f0a50ddf55
Amadou <amadou_dieye@hotmail.fr>
2020-09-21 10:06:51 +0200

    ajouts exo21 et debut exo22

---
+++ exo22.js
@@ -0,0 +1,6 @@
+function splice(str='',start,deleteCountopt=str.length-start,toAddopt='') {
+    str=''
+}
+console.log(splice('Tony Tony Ch0pper',0,10))
+console.log(splice('Tony Ch0pper',0,4,'Tony Tony'))
+console.log(splice('Tony Tony Usopp',-5,5,'Ch0pper'))
