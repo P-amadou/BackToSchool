1aec788db1b6bfcdb94005b43d9b67027a4302ea
Amadou <amadou_dieye@hotmail.fr>
2020-09-21 11:40:35 +0200

    ajouts exo 22 a 24

--- exo22.js
+++ exo22.js
@@ -1,5 +1,14 @@
+//Attendre correction
 function splice(str='',start,deleteCountopt=str.length-start,toAddopt='') {
-    str=''
+let res=toAddopt
+let i= start
+
+for (i = start; i < str.length; i++) {
+    if(i>=deleteCountopt){
+        res+=str[i]
+    }
+}
+    return res
 }
 console.log(splice('Tony Tony Ch0pper',0,10))
 console.log(splice('Tony Ch0pper',0,4,'Tony Tony'))
